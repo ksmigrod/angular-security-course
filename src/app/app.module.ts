4f4c80700761763f36bc1e44686dbd3415803f8f
Krzysztof Śmigrodzki <ksmigrod@gmail.com>
2019-05-29 13:03:53 +0200

    And of section 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { LessonsComponent } from './lessons/lessons.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import {AppComponent} from './app.component';
+import {LessonsComponent} from './lessons/lessons.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 import {RouterModule} from "@angular/router";
 import {routesConfig} from "./routes.config";
 import {LessonsService} from "./services/lessons.service";
 import {ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "./services/auth.service";
 
-
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/shareReplay';
+import 'rxjs/add/operator/do';
 
 @NgModule({
   declarations: [
@@ -26,7 +30,7 @@ import {ReactiveFormsModule} from "@angular/forms";
       RouterModule.forRoot(routesConfig),
       ReactiveFormsModule
   ],
-  providers: [LessonsService],
+  providers: [LessonsService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
